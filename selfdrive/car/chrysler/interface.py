48012387e99450fc84df5a8a1b9e102c85f595cd
VipinM <vipin.mahajan@outlook.com>
2021-04-15 22:37:46 -0400

    Allow 2019 cars to steer down to 14.5 m/s (33mph) if already engaged.

--- selfdrive/car/chrysler/interface.py
+++ selfdrive/car/chrysler/interface.py
@@ -4,6 +4,9 @@ from selfdrive.car.chrysler.values import CAR
 from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint
 from selfdrive.car.interfaces import CarInterfaceBase
 
+# Logic in update() relies on these values being different.
+HIGH_MIN_2019 = 17.5  # m/s minimum for steering engage on 2019.
+LOW_MIN_2019 = 14.5  # m/s minimum after already engaged on 2019. 14.5m/s=33mph
 
 class CarInterface(CarInterfaceBase):
   @staticmethod
@@ -69,6 +72,15 @@ class CarInterface(CarInterfaceBase):
     # events
     events = self.create_common_events(ret, extra_gears=[car.CarState.GearShifter.low],
                                        gas_resume_speed=2.)
+
+    # allow 2019 cars to steer down to 14.5 m/s if already engaged.
+    if ret.cruiseState.enabled: 
+      if ret.vEgo > HIGH_MIN_2019:
+        self.CP.minSteerSpeed = LOW_MIN_2019
+      elif ret.vEgo < LOW_MIN_2019:
+        self.CP.minSteerSpeed = HIGH_MIN_2019
+    else: 
+      self.CP.minSteerSpeed = HIGH_MIN_2019
 
     if ret.vEgo < self.CP.minSteerSpeed:
       events.add(car.CarEvent.EventName.belowSteerSpeed)
